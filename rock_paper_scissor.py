923c1e8195d4e69e8c9c5f36075de987481d8f9d
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2025-01-07 10:10:29 +0530

    refactor: Modularize rock-paper-scissors game for improved code clarity

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -5,16 +5,19 @@ emojis = {'r': '🪨',
             'p': '📄', 
             's': '✂️'}
     
-while True:
-    user_choice = input('Rock, paper, or scissors? (r/p/s): ')
-    if user_choice not in valid_choices:
-        print('Invalid choice')
-        continue
+def get_user_choice():
+    while True:
+        user_choice = input('Rock, paper, or scissors? (r/p/s): ')
+        if user_choice in valid_choices:
+            return user_choice
+        else:
+            print('Invalid choice')
 
-    computer_choice = random.choice(valid_choices)
+def print_choices(user_choice, computer_choice):
     print(f'You chose {emojis[user_choice]}')
     print(f'Computer choice {emojis[computer_choice]}')
 
+def determine_winner(user_choice, computer_choice):
     if ((user_choice == 'r' and computer_choice == 's') or 
         (user_choice == 'p' and computer_choice == 'r') or 
         (user_choice == 's' and computer_choice == 'p')):
@@ -24,7 +27,18 @@ while True:
     else:
         print('You lose')
 
-    continue_choice = input('Do you want to continue? (y/n): ')
-    if continue_choice.lower() == 'n':
-        break
-    
+def start_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(valid_choices)
+
+        print_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        continue_choice = input('Do you want to continue? (y/n): ').lower()
+        if continue_choice == 'n':
+            break
+
+start_game()
